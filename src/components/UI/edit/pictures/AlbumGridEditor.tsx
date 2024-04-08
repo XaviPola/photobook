03e34057ce17621aa8698458e15bc3b67231b114
xvpl <xavi.pola@gmail.com>
2024-04-08 11:03:16 +0200

    wip-local-backend

--- src/components/UI/edit/pictures/AlbumGridEditor.tsx
+++ src/components/UI/edit/pictures/AlbumGridEditor.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import type { FC } from 'react';
 import {
     DndContext,
@@ -10,41 +10,73 @@ import {
     useSensors,
 } from '@dnd-kit/core';
 import type { DragStartEvent, DragEndEvent } from '@dnd-kit/core';
-import { arrayMove, SortableContext, rectSortingStrategy } from '@dnd-kit/sortable';
+import { SortableContext, rectSortingStrategy } from '@dnd-kit/sortable';
 import Grid from '@components/UI/edit/pictures/Grid.tsx';
 import SortablePictureCard from '@components/UI/edit/pictures/SortablePictureCard.tsx';
 import PictureCard from '@components/UI/edit/pictures/PictureCard.tsx';
 
 const AlbumGridEditor: FC = () => {
-    const [items, setItems] = useState(Array.from({ length: 8 }, (_, i) => (i + 1).toString()));
+    const [pictures, setPictures] = useState([]);
+
+    const [refresh, setRefresh] = useState(true);
+
+    useEffect(() => {
+        if (refresh) {
+            console.log('fetching pictures');
+            fetch('http://localhost:1234/myAlbum/pictures')
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    setPictures(data.pictures);
+                    setRefresh(false);
+                });
+        }
+    }, [refresh]);
+
     const [activeId, setActiveId] = useState<string | null>(null);
-    const sensors = useSensors(useSensor(MouseSensor, {
-        activationConstraint: {
-          distance: 8,
+
+
+    const sensors = useSensors(
+        useSensor(MouseSensor, {
+            activationConstraint: {
+            distance: 8,
         },
-      }), useSensor(TouchSensor, {
-        activationConstraint: {
-          distance: 8,
-        },
-      }));
+        }), 
+        useSensor(TouchSensor, {
+            activationConstraint: {
+                distance: 8,
+            },
+        }));
       
     const handleDragStart = useCallback((event: DragStartEvent) => {
         setActiveId(event.active.id.toString());
     }, []);
+    
+    function getIndexById(pictureId: string): number {
+        return pictures.findIndex((picture) => picture.id.toString() === pictureId);
+    }
+    
     const handleDragEnd = useCallback((event: DragEndEvent) => {
         const { active, over } = event;
 
         if (active.id !== over?.id) {
-            setItems((items) => {
-                const oldIndex = items.indexOf(active.id.toString());
-                const newIndex = items.indexOf(over!.id.toString());
+            setPictures((pictures) => {
+                const oldIndex = getIndexById(active.id.toString());
+                const newIndex = getIndexById(over!.id.toString());
 
-                return arrayMove(items, oldIndex, newIndex);
+                const newPictures = [...pictures];
+
+                newPictures.splice(oldIndex, 1);
+                newPictures.splice(newIndex, 0, pictures[oldIndex]);
+
+                return newPictures;
             });
         }
 
         setActiveId(null);
-    }, []);
+    }, [pictures]);
+    
     const handleDragCancel = useCallback(() => {
         setActiveId(null);
     }, []);
@@ -57,10 +89,10 @@ const AlbumGridEditor: FC = () => {
             onDragEnd={handleDragEnd}
             onDragCancel={handleDragCancel}
         >
-            <SortableContext items={items} strategy={rectSortingStrategy}>
+            <SortableContext items={pictures} strategy={rectSortingStrategy}>
                 <Grid >
-                    {items.map((id, index) => (
-                        <SortablePictureCard key={id} id={id} imgPath={`/demo_images/${id}.png`} imgNumberInAlbum={(index + 1).toString()}/>
+                    {pictures.map((picture, index) => (
+                        <SortablePictureCard key={picture.id} id={picture.id} imgDescription={picture.description} imgTitle={picture.title} imgPath={picture.url} imgNumberInAlbum={(index + 1).toString()}/>
                     ))}
                 </Grid>
                 <DragOverlay adjustScale style={{ transformOrigin: '0 0 ' }}>
