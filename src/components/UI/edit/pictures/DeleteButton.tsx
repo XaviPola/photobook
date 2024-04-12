3f8c6897f1413d1bfe9cdc32bf694e46547c2803
xvpl <xavi.pola@gmail.com>
2024-04-12 14:41:35 +0200

    first version of delete button component

---
+++ src/components/UI/edit/pictures/DeleteButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { HTMLAttributes, CSSProperties } from 'react';
+import BasicButtonComponent from './BasicButton';
+import DeleteIcon from '@components/UI/icons/Delete';
+
+export type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
+    onClick?: () => void;
+    extraStyles?: CSSProperties;
+    };
+
+const DeleteButton = ({ onClick, extraStyles }: ButtonProps) => {    
+    const deleteButtonStyles = {
+        display: "flex",
+        width: "26px",
+        height: "26px",
+        border: "none",
+        padding: "0",
+        margin: "0",
+        backgroundColor: "transparent",
+        justifyContent: "center",
+        alignItems: "center",
+    };
+
+    const styles = extraStyles !== undefined ? { ...deleteButtonStyles, ...extraStyles } : deleteButtonStyles;
+        
+    return (
+            <BasicButtonComponent element={<DeleteIcon />} onClick={onClick} extraStyles={styles} />
+        );
+}
+export default DeleteButton;
