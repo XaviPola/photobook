aa38f7bd9947e5d3822d65670388eb62f37adc3c
xvpl <xavi.pola@gmail.com>
2024-04-23 00:08:53 +0200

    feat: load from infra initial cover setup

--- src/components/UI/edit/cover/CoverEditor.tsx
+++ src/components/UI/edit/cover/CoverEditor.tsx
@@ -1,49 +1,124 @@
 import React, { useState, useEffect } from 'react'
 import ColorSelector from './ColorSelector'
 import CoverPreview from './CoverPreview'
+import axios from 'axios'
 
 interface CoverEditorProps {
   albumId: string
-  title?: string
-  author?: string
-  fontColor?: string
-  backgroundColor?: string
 }
 
-const CoverEditor: React.FC<CoverEditorProps> = ({ albumId, title: initialTitle = 'A Title', author: initialAuthor = 'Xavi F Pola', fontColor: initialFontColor = '#000000', backgroundColor: initialBackgroundColor = '#e12356' }) => {
-  const [title, setTitle] = useState(initialTitle)
-  const [author, setAuthor] = useState(initialAuthor)
-  const [fontColor, setFontColor] = useState(initialFontColor)
-  const [backgroundColor, setBackgroundColor] = useState(initialBackgroundColor)
+const CoverEditor: React.FC<CoverEditorProps> = ({ albumId }) => {
+  const defaultbackgroundColor = '#000000'
+  const defaultFontColor = '#ffffff'
+
+  const [title, setTitle] = useState<string | undefined>()
+  const [author, setAuthor] = useState<string | undefined>()
+  const [fontColor, setFontColor] = useState<string>(defaultFontColor)
+  const [backgroundColor, setBackgroundColor] = useState<string>(defaultbackgroundColor)
   const [coverPicture, setCoverPicture] = useState<File | null>(null)
+  const [imgPath, setImgPath] = useState<string | undefined>()
+
+  const createDefaultCover = async (albumId: string): Promise<void> => {
+    axios.post(`http://localhost:1234/covers/${albumId}`, {
+      backgroundColor: defaultbackgroundColor,
+      fontColor: defaultFontColor
+    })
+      .then((response) => {
+        console.log('Cover created successfully')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
   useEffect(() => {
-    console.log('CoverEditor mounted')
-  }, [title, author, fontColor, backgroundColor, coverPicture])
+    const getSetupCover = async (albumId: string): Promise<void> => {
+      await axios.get(`http://localhost:1234/covers/${albumId}`)
+        .then((response) => {
+          const { imgPath, title, author, backgroundColor, fontColor } = response.data
+          setImgPath(imgPath)
+          setBackgroundColor(backgroundColor)
+          setFontColor(fontColor)
+          setTitle(title)
+          setAuthor(author)
+        })
+        .catch(async (error) => {
+          if (error.response.status === 404) {
+            console.log('Cover not found, creating default cover')
+            await createDefaultCover(albumId)
+            setBackgroundColor(defaultbackgroundColor)
+            setFontColor(defaultFontColor)
+          }
+        })
+    }
+    void getSetupCover(albumId)
+  }, [])
 
-  const handleBackgroundColorChange = (color: string): void => {
-    console.log('backgroundColor:', color)
-    setBackgroundColor(color)
+  const updateCoverColors = (backgroundColor: string, fontColor: string): void => {
+    axios.put(`http://localhost:1234/covers/${albumId}`, { backgroundColor, fontColor })
+      .then((response) => {
+        console.log('Colors updated successfully')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
-  const handleFontColorChange = (color: string): void => {
-    console.log('fontColor:', color)
-    setFontColor(color)
+  useEffect(() => {
+    updateCoverColors(backgroundColor, fontColor)
+  }, [backgroundColor, fontColor])
+
+  const updateTitle = (title: string): void => {
+    axios.put(`http://localhost:1234/albums/${albumId}`, { title })
+      .then((response) => {
+        console.log('Title updated successfully')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
-  const handleDrop = (event: React.DragEvent<HTMLDivElement>): void => {
-    event.preventDefault()
-    // Your logic to handle dropping the cover picture
+  const updateAuthor = (author: string): void => {
+    axios.put(`http://localhost:1234/albums/${albumId}`, { author })
+      .then((response) => {
+        console.log('Author updated successfully')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
-  const handleDragOver = (event: React.DragEvent<HTMLDivElement>): void => {
-    event.preventDefault()
-    // Your logic to handle drag over
+  useEffect(() => {
+    if (title !== undefined) {
+      updateTitle(title)
+    }
+    if (author !== undefined) {
+      updateAuthor(author)
+    }
+  }, [title, author])
+
+  const updateCoverPicture = (): void => {
+    console.log('uploading cover image:', coverPicture)
+    const formData = new FormData()
+    formData.append('image', coverPicture as Blob)
+    axios.patch(`http://localhost:1234/covers/${albumId}/image`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+      .then((response) => {
+        console.log('Cover image updated successfully')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
-  const handleDragLeave = (): void => {
-    // Your logic to handle drag leave
-  }
+  useEffect(() => {
+    if (coverPicture !== null) {
+      updateCoverPicture()
+    }
+  }, [coverPicture])
 
   return (
     <>
@@ -52,20 +127,16 @@ const CoverEditor: React.FC<CoverEditorProps> = ({ albumId, title: initialTitle 
         author={author}
         fontColor={fontColor}
         backgroundColor={backgroundColor}
-        imgPath={coverPicture != null ? URL.createObjectURL(coverPicture) : undefined}
+        imgPath={(imgPath !== null) ? imgPath : undefined}
+        setCoverPicture={setCoverPicture}
         setTitle={setTitle}
         setAuthor={setAuthor}
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-        onDragLeave={handleDragLeave}
       />
       <ColorSelector
         backgroundColor={backgroundColor}
         fontColor={fontColor}
         setBackgroundColor={setBackgroundColor}
         setFontColor={setFontColor}
-        onBackgroundColorChange={handleBackgroundColorChange}
-        onFontColorChange={handleFontColorChange}
       />
 
     </>
