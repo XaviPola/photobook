5cace882574c46202f7aaae6fe9fc64f5120ad23
xvpl <xavi.pola@gmail.com>
2024-04-23 12:51:38 +0200

    refactor: color selector to be a reusable component

--- src/components/UI/edit/cover/ColorSelector.tsx
+++ src/components/UI/edit/cover/ColorSelector.tsx
@@ -2,36 +2,12 @@ import React from 'react'
 import type { CSSProperties } from 'react'
 
 interface ColorSelectorProps {
-  backgroundColor: string
-  fontColor: string
-  setBackgroundColor: React.Dispatch<React.SetStateAction<string>>
-  setFontColor: React.Dispatch<React.SetStateAction<string>>
+  colorCopy: string
+  colorInitialValue: string
+  onColorChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const ColorSelector: React.FC<ColorSelectorProps> = ({ backgroundColor, fontColor, setBackgroundColor, setFontColor }) => {
-  const onBackgroundColorChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const color = event.target.value
-    console.log('backgroundColor:', color)
-    setBackgroundColor(color)
-  }
-
-  const onFontColorChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const color = event.target.value
-    console.log('fontColor:', color)
-    setFontColor(color)
-  }
-
-  const boxStyles: CSSProperties = {
-    display: 'flex',
-    height: '100%',
-    flexDirection: 'row',
-    gap: '40px',
-    borderRadius: '8px',
-    boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)',
-    padding: '20px',
-    justifyContent: 'center'
-  }
-
+const ColorSelector: React.FC<ColorSelectorProps> = ({ colorCopy, colorInitialValue, onColorChange }) => {
   const labelStyles: CSSProperties = {
     textTransform: 'capitalize',
     fontFamily: '"Roboto", sans-serif',
@@ -44,19 +20,16 @@ const ColorSelector: React.FC<ColorSelectorProps> = ({ backgroundColor, fontColo
     display: 'flex',
     flexDirection: 'row',
     gap: '10px',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    borderRadius: '8px',
+    border: '1px solid #ccc',
+    padding: '10px'
   }
 
   return (
-    <div style={boxStyles}>
-      <div style={inputStyles}>
-        <label style={labelStyles}>Background</label>
-        <input type='color' value={backgroundColor} onChange={onBackgroundColorChange} />
-      </div>
-      <div style={inputStyles}>
-        <label style={labelStyles}>Font</label>
-        <input type='color' value={fontColor} onChange={onFontColorChange} />
-      </div>
+    <div style={inputStyles}>
+      <label style={labelStyles}>{colorCopy}</label>
+      <input type='color' value={colorInitialValue} onChange={onColorChange} />
     </div>
   )
 }
